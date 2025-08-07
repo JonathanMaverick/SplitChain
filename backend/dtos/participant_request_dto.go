60790d7beec9e43c028cdf66c9bbba9bdbc2db52
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2025-08-07 22:58:17 +0700

    feat: test

--- backend/dtos/participant_request_dto.go
+++
@@ -1,6 +0,0 @@
-package dtos
-
-type GetParticipantDetailRequest struct {
-	BillID        string `json:"billId" binding:"required"`
-	ParticipantID string `json:"participantId" binding:"required"`
-}
--- backend/models/user.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type User struct {
-	WalletAddress string `gorm:"primaryKey;type:varchar(255)" json:"wallet_address"`
-}
